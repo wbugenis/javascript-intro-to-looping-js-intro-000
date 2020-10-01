9af846543e11690fd632c26af6faa1b22718d305
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-01 02:47:20 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,8 +3,7 @@ function forLoop(array) {
   for (let i = 0; i < 25; i++) {
    if (i === 1) {
       array.push("I am 1 strange loop.")
-      else array.push(`I am ${i} strange loops.`)
-    }
+   } else array.push(`I am ${i} strange loops.`)
   }
   return array;
 }
