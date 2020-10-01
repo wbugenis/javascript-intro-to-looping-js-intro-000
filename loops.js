d21a801b55c60b534f5c4320aceca27cdc91bfc4
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-01 02:45:19 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,3 +1,9 @@
+var array = [];
 for (let i = 0; i < 25; i++) {
-  
+  if (i === 1) {
+    array.push("I am 1 strange loop.")
+    else array.push(`I am ${i} strange loops.`)
+  }
 }
+
+return array;
