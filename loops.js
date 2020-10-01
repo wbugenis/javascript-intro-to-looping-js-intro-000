a104582ef3bcf65e3020a7e45334db653d757c80
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-01 02:59:21 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -28,5 +28,5 @@ function incrementVariable() {
 function doWhileLoop(num) {
   do{
     console.log("I run once regardless.");
-  }while (incrementVariable() < num);
+  } while (incrementVariable() < num);
 }
